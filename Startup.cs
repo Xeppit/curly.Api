cd02787e5e1d0b2f188aab7092fe038589250516
Nathan Wilson <nathan@xeppit.co.uk>
2019-12-02 22:35:29 +0000

    A

--- Startup.cs
+++ Startup.cs
@@ -30,13 +30,12 @@ namespace curly.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
 
-                        // requires using Microsoft.Extensions.Options
+            // requires using Microsoft.Extensions.Options
             services.Configure<DatabaseSettings>(
                 Configuration.GetSection(nameof(DatabaseSettings)));
 
